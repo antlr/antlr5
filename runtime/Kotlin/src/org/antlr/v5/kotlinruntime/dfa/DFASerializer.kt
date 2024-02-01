ba648592fcebb32bc73d9120146fb009c5401d74
Federico Tomassetti <federico@strumenta.com>
2024-02-01 13:50:15 +0100

    Update Strumenta Copyright into ANTLR Copyright
    Signed-off-by: Federico Tomassetti <federico@strumenta.com>

--- runtime/Kotlin/src/org/antlr/v5/kotlinruntime/dfa/DFASerializer.kt
+++ runtime/Kotlin/src/org/antlr/v5/kotlinruntime/dfa/DFASerializer.kt
@@ -1,5 +1,8 @@
-// Copyright 2017-present Strumenta and contributors, licensed under Apache 2.0.
-// Copyright 2024-present Strumenta and contributors, licensed under BSD 3-Clause.
+/*
+ * Copyright (c) 2012-present The ANTLR Project. All rights reserved.
+ * Use of this file is governed by the BSD 3-clause license that
+ * can be found in the LICENSE.txt file in the project root.
+ */
 
 package org.antlr.v5.kotlinruntime.dfa
 
