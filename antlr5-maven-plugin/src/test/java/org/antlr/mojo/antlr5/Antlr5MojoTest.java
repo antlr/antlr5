f76ab4b05efdc46b4e07f845fe1993967882dbdc
Federico Tomassetti <federico@strumenta.com>
2024-02-03 13:31:24 +0100

    Rename Antlr4MojoTest into Antlr5MojoTest
    Signed-off-by: Federico Tomassetti <federico@strumenta.com>

--- antlr5-maven-plugin/src/test/java/org/antlr/mojo/antlr5/Antlr5MojoTest.java
+++ antlr5-maven-plugin/src/test/java/org/antlr/mojo/antlr5/Antlr5MojoTest.java
@@ -27,7 +27,7 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 
-public class Antlr4MojoTest {
+public class Antlr5MojoTest {
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
