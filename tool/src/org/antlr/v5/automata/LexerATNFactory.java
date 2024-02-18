antlr/antlr4

Implement redundant epsilon removing optimization
* Introduced `RedundantEpsilonRemover` that performs the following ATN optimizations (described below):
  * Removing of single incoming epsilon transition
  * Removing of single outgoing epsilon transition with several incoming transitions
* Removed all optimizations from `ParserATNFactory` since they are useless, not fully correct and are performed on the separated optimization step
* Introduced `ATNOptimizerHelper` that do the following:
  * Calculates incoming transitions that are used in ATN optimizers
  * Tracks replacement of state that are being removed during optimization. Old states are being replaced on the new ones in the final step (`updateAstNodes`)
  * Compresses array of ATN states (removes null items after previous optimization steps, `compressStates`)
* Fixed the previous `ATNOptimizer` and renamed to `SetMerger`. Now it considers incoming transitions and `ATNOptimizerHelper` accurately tracks replacements.

Implemented optimizations decreases ATN especially for lexers and should improve performance for generated parsers because of decreased number of method calls. Also they don't affect runtime code except of interpreter part (that is buggy anyway).

Signed-off-by: Ivan Kochurkin <kvanttt@gmail.com>

tool/src/org/antlr/v4/automata/LexerATNFactory.java
@@ -9,6 +9,7 @@ package org.antlr.v4.automata;
 import org.antlr.runtime.CommonToken;
 import org.antlr.runtime.Token;
 import org.antlr.runtime.tree.CommonTree;
+import org.antlr.v4.automata.optimization.ATNOptimizer;
 import org.antlr.v4.codegen.CodeGenerator;
 import org.antlr.v4.misc.CharSupport;
 import org.antlr.v4.misc.EscapeSequenceParsing;

tool/src/org/antlr/v4/automata/ParserATNFactory.java
@@ -12,6 +12,7 @@ import org.antlr.runtime.Token;
 import org.antlr.runtime.tree.CommonTreeNodeStream;
 import org.antlr.runtime.tree.Tree;
 import org.antlr.v4.analysis.LeftRecursiveRuleTransformer;
+import org.antlr.v4.automata.optimization.ATNOptimizer;
 import org.antlr.v4.misc.CharSupport;
 import org.antlr.v4.parse.ANTLRParser;
 import org.antlr.v4.parse.ATNBuilder;
@@ -442,10 +443,6 @@ public class ParserATNFactory implements ATNFactory {
 			// hook alts up to decision block
 			epsilon(start, alt.left);
 			epsilon(alt.right, end);
-			// no back link in ATN so must walk entire alt to see if we can
-			// strip out the epsilon to 'end' state
-			TailEpsilonRemover opt = new TailEpsilonRemover(atn);
-			opt.visit(alt.left);
 		}
 		Handle h = new Handle(start, end);
 //		FASerializer ser = new FASerializer(g, h.left);
@@ -464,44 +461,22 @@ public class ParserATNFactory implements ATNFactory {
 
 	public Handle elemList(List<Handle> els) {
 		int n = els.size();
-		for (int i = 0; i < n - 1; i++) {	// hook up elements (visit all but last)
-			Handle el = els.get(i);
-			// if el is of form o-x->o for x in {rule, action, pred, token, ...}
-			// and not last in alt
-            Transition tr = null;
-            if ( el.left.getNumberOfTransitions()==1 ) tr = el.left.transition(0);
-            boolean isRuleTrans = tr instanceof RuleTransition;
-            if ( el.left.getStateType() == ATNState.BASIC &&
-				el.right != null &&
-				el.right.getStateType()== ATNState.BASIC &&
-				tr!=null && (isRuleTrans && ((RuleTransition)tr).followState == el.right || tr.target == el.right) ) {
-				// we can avoid epsilon edge to next el
-				Handle handle = null;
-				if (i + 1 < els.size()) {
-					handle = els.get(i + 1);
-				}
-				if (handle != null) {
-					if (isRuleTrans) {
-						((RuleTransition) tr).followState = handle.left;
-					} else {
-						tr.target = handle.left;
-					}
-				}
-				atn.removeState(el.right); // we skipped over this state
-			}
-			else { // need epsilon if previous block's right end node is complicated
-				epsilon(el.right, els.get(i+1).left);
+		Handle first = els.get(0);
+		Handle nextEl = first;
+		for (int i = 1; i < n; i++) {
+			Handle el = nextEl;
+			nextEl = els.get(i);
+			if (el != null && nextEl != null) {
+				epsilon(el.right, nextEl.left);
 			}
 		}
-		Handle first = els.get(0);
-		Handle last = els.get(n - 1);
 		ATNState left = null;
 		if (first != null) {
 			left = first.left;
 		}
 		ATNState right = null;
-		if (last != null) {
-			right = last.right;
+		if (nextEl != null) {
+			right = nextEl.right;
 		}
 		return new Handle(left, right);
 	}

tool/src/org/antlr/v4/automata/optimization/InTransition.java
@@ -1,0 +1,39 @@
+package org.antlr.v4.automata.optimization;
+
+import org.antlr.v4.runtime.atn.ATNState;
+import org.antlr.v4.runtime.atn.Transition;
+
+import java.util.Objects;
+
+/**
+ * Helper class used in ATN optimizers
+ */
+class InTransition {
+	public final Transition transition;
+	public final ATNState previousState;
+	public final boolean isFollowState;
+
+	public InTransition(Transition transition, ATNState previousState, boolean isFollowState) {
+		this.transition = transition;
+		this.previousState = previousState;
+		this.isFollowState = isFollowState;
+	}
+
+	@Override
+	public String toString() {
+		return previousState + " -> " + transition.target;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		InTransition that = (InTransition) o;
+		return isFollowState == that.isFollowState && Objects.equals(transition, that.transition) && Objects.equals(previousState, that.previousState);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(transition, previousState, isFollowState);
+	}
+}

tool/src/org/antlr/v4/automata/optimization/RedundantEpsilonRemover.java
@@ -1,0 +1,177 @@
+package org.antlr.v4.automata.optimization;
+
+import org.antlr.v4.runtime.atn.*;
+
+import java.util.*;
+
+public class RedundantEpsilonRemover {
+	public final ATN atn;
+
+	private final ATNOptimizerHelper helper;
+
+	private RedundantEpsilonRemover(ATN atn, ATNOptimizerHelper helper) {
+		this.atn = atn;
+		this.helper = helper;
+	}
+
+	public static int optimize(ATNOptimizerHelper helper) {
+		List<ATNState> removedStates = helper.getRemovedStates();
+		int numberOfRemovedStates = removedStates.size();
+		new RedundantEpsilonRemover(helper.atn, helper).traverseATN();
+		return removedStates.size() - numberOfRemovedStates;
+	}
+
+	private void traverseATN() {
+		Set<ATNState> visitedStates = new HashSet<>();
+		for (RuleStartState start : atn.ruleToStartState) {
+			traverseState(start, visitedStates);
+		}
+	}
+
+	private void traverseState(ATNState state, Set<ATNState> visitedStates) {
+		if (!visitedStates.add(state)) {
+			return;
+		}
+
+		optimizeState(state);
+
+		for (int i = 0; i < state.getNumberOfTransitions(); i++) {
+			Transition transition = state.transition(i);
+			traverseState(transition.target, visitedStates);
+			if (transition instanceof RuleTransition) {
+				traverseState(((RuleTransition) transition).followState, visitedStates);
+			}
+		}
+	}
+
+	private void optimizeState(ATNState state) {
+		if (state instanceof BasicState && state.getNumberOfTransitions() == 1) {
+			Transition outTransition = state.transition(0);
+			Boolean removeInTransition = null;
+
+			List<InTransition> inTransitions = helper.getInTransitions(state);
+			InTransition inTransition0 = inTransitions.get(0);
+
+			if (inTransitions.size() == 1) {
+				// Input:
+				//     ε     t
+				// I1 --> S --> O1
+
+				// Output:
+				//     t
+				// I1 --> O1
+				if (isSafeForRemovingEpsilon(inTransition0.transition) && validateInTransitionForRemoving(inTransition0, outTransition)) {
+					helper.removeInTransitionsForOutStates(state);
+					helper.replaceTransition(inTransition0, outTransition);
+					removeInTransition = true;
+				}
+			}
+
+			if (removeInTransition == null && isSafeForRemovingEpsilon(outTransition)) {
+				// State with several incoming transitions of any kind can be safely removed
+
+				// Input:
+				// I1
+				//    \     ε
+				// I2 -> S --> O1
+				//    /
+				// I3
+
+				// Output:
+				// I1
+				//    \
+				// I2 -> O1
+				//    /
+				// I3
+				helper.removeInTransitionsForOutStates(state);
+				for (InTransition inTransition : inTransitions) {
+					Transition newTransition = cloneTransition(inTransition, outTransition.target);
+					helper.replaceTransition(inTransition, newTransition);
+				}
+				removeInTransition = false;
+			}
+
+			if (removeInTransition != null) {
+				ATNState newState;
+				if (removeInTransition) {
+					newState = inTransition0.previousState;
+				} else {
+					newState = outTransition.target;
+				}
+
+				helper.addReplacement(state, newState);
+			}
+		}
+	}
+
+	private static boolean isSafeForRemovingEpsilon(Transition transition) {
+		return transition instanceof EpsilonTransition && ((EpsilonTransition) transition).outermostPrecedenceReturn() == -1;
+	}
+
+	private static boolean validateInTransitionForRemoving(InTransition inTransition, Transition outTransition) {
+		Boolean onlyEpsilonTransitions = null;
+		ATNState prevState = inTransition.previousState;
+		Transition transitionToRemove = inTransition.transition;
+		for (int i = 0; i < prevState.getNumberOfTransitions(); i++) {
+			Transition t = prevState.transition(i);
+			if (t != transitionToRemove) {
+				if (!t.isEpsilon()) {
+					onlyEpsilonTransitions = false;
+					break;
+				}
+				onlyEpsilonTransitions = true;
+			}
+		}
+
+		if (onlyEpsilonTransitions != null && onlyEpsilonTransitions) {
+			return outTransition instanceof EpsilonTransition;
+		}
+		else {
+			return true;
+		}
+	}
+
+	private static Transition cloneTransition(InTransition inTransition, ATNState newTarget) {
+		Transition transition = inTransition.transition;
+		switch (transition.getSerializationType()) {
+			case Transition.EPSILON:
+				return new EpsilonTransition(newTarget, ((EpsilonTransition) transition).outermostPrecedenceReturn());
+			case Transition.RANGE:
+				RangeTransition rangeTransition = (RangeTransition) transition;
+				return new RangeTransition(newTarget, rangeTransition.from, rangeTransition.to);
+			case Transition.RULE:
+				RuleTransition ruleTransition = (RuleTransition) transition;
+				RuleStartState ruleStart;
+				ATNState followState;
+				if (inTransition.isFollowState) {
+					ruleStart = (RuleStartState) ruleTransition.target;
+					followState = newTarget;
+				}
+				else {
+					ruleStart = (RuleStartState) newTarget;
+					followState = ruleTransition.followState;
+				}
+				return new RuleTransition(ruleStart, ruleTransition.ruleIndex, ruleTransition.precedence, followState);
+			case Transition.PREDICATE:
+				PredicateTransition predicateTransition = (PredicateTransition) transition;
+				return new PredicateTransition(newTarget, predicateTransition.ruleIndex,
+						predicateTransition.predIndex, predicateTransition.isCtxDependent);
+			case Transition.PRECEDENCE:
+				return new PrecedencePredicateTransition(newTarget, ((PrecedencePredicateTransition) transition).precedence);
+			case Transition.ATOM:
+				return new AtomTransition(newTarget, ((AtomTransition) transition).label);
+			case Transition.ACTION:
+				ActionTransition actionTransition = (ActionTransition) transition;
+				return new ActionTransition(newTarget, actionTransition.ruleIndex, actionTransition.actionIndex,
+						actionTransition.isCtxDependent);
+			case Transition.SET:
+				return new SetTransition(newTarget, ((SetTransition) transition).set);
+			case Transition.NOT_SET:
+				return new NotSetTransition(newTarget, ((NotSetTransition) transition).set);
+			case Transition.WILDCARD:
+				return new WildcardTransition(newTarget);
+		}
+		assert true : "Transition should exist";
+		return null;
+	}
+}

antlr/antlr5

tool/src/org/antlr/v5/automata/LexerATNFactory.java
@@ -9,6 +9,7 @@ package org.antlr.v5.automata;
 import org.antlr.runtime.CommonToken;
 import org.antlr.runtime.Token;
 import org.antlr.runtime.tree.CommonTree;
+import org.antlr.v5.automata.optimization.ATNOptimizer;
 import org.antlr.v5.codegen.CodeGenerator;
 import org.antlr.v5.misc.CharParseResult;
 import org.antlr.v5.misc.GrammarLiteralParser;

tool/src/org/antlr/v5/automata/ParserATNFactory.java
@@ -12,6 +12,7 @@ import org.antlr.runtime.Token;
 import org.antlr.runtime.tree.CommonTreeNodeStream;
 import org.antlr.runtime.tree.Tree;
 import org.antlr.v5.analysis.LeftRecursiveRuleTransformer;
+import org.antlr.v5.automata.optimization.ATNOptimizer;
 import org.antlr.v5.parse.ANTLRParser;
 import org.antlr.v5.parse.ATNBuilder;
 import org.antlr.v5.parse.GrammarASTAdaptor;
@@ -430,10 +431,6 @@ public class ParserATNFactory implements ATNFactory {
 			// hook alts up to decision block
 			epsilon(start, alt.left);
 			epsilon(alt.right, end);
-			// no back link in ATN so must walk entire alt to see if we can
-			// strip out the epsilon to 'end' state
-			TailEpsilonRemover opt = new TailEpsilonRemover(atn);
-			opt.visit(alt.left);
 		}
 		Handle h = new Handle(start, end);
 //		FASerializer ser = new FASerializer(g, h.left);
@@ -452,44 +449,22 @@ public class ParserATNFactory implements ATNFactory {
 
 	public Handle elemList(List<Handle> els) {
 		int n = els.size();
-		for (int i = 0; i < n - 1; i++) {	// hook up elements (visit all but last)
-			Handle el = els.get(i);
-			// if el is of form o-x->o for x in {rule, action, pred, token, ...}
-			// and not last in alt
-            Transition tr = null;
-            if ( el.left.getNumberOfTransitions()==1 ) tr = el.left.transition(0);
-            boolean isRuleTrans = tr instanceof RuleTransition;
-            if ( el.left.getStateType() == ATNState.BASIC &&
-				el.right != null &&
-				el.right.getStateType()== ATNState.BASIC &&
-				tr!=null && (isRuleTrans && ((RuleTransition)tr).followState == el.right || tr.target == el.right) ) {
-				// we can avoid epsilon edge to next el
-				Handle handle = null;
-				if (i + 1 < els.size()) {
-					handle = els.get(i + 1);
-				}
-				if (handle != null) {
-					if (isRuleTrans) {
-						((RuleTransition) tr).followState = handle.left;
-					} else {
-						tr.target = handle.left;
-					}
-				}
-				atn.removeState(el.right); // we skipped over this state
-			}
-			else { // need epsilon if previous block's right end node is complicated
-				epsilon(el.right, els.get(i+1).left);
+		Handle first = els.get(0);
+		Handle nextEl = first;
+		for (int i = 1; i < n; i++) {
+			Handle el = nextEl;
+			nextEl = els.get(i);
+			if (el != null && nextEl != null) {
+				epsilon(el.right, nextEl.left);
 			}
 		}
-		Handle first = els.get(0);
-		Handle last = els.get(n - 1);
 		ATNState left = null;
 		if (first != null) {
 			left = first.left;
 		}
 		ATNState right = null;
-		if (last != null) {
-			right = last.right;
+		if (nextEl != null) {
+			right = nextEl.right;
 		}
 		return new Handle(left, right);
 	}

tool/src/org/antlr/v5/automata/optimization/InTransition.java
@@ -1,0 +1,45 @@
+/*
+ * Copyright (c) 2012-present The ANTLR Project. All rights reserved.
+ * Use of this file is governed by the BSD 3-clause license that
+ * can be found in the LICENSE.txt file in the project root.
+ */
+
+package org.antlr.v5.automata.optimization;
+
+import org.antlr.v5.runtime.atn.ATNState;
+import org.antlr.v5.runtime.atn.Transition;
+
+import java.util.Objects;
+
+/**
+ * Helper class used in ATN optimizers
+ */
+class InTransition {
+	public final Transition transition;
+	public final ATNState previousState;
+	public final boolean isFollowState;
+
+	public InTransition(Transition transition, ATNState previousState, boolean isFollowState) {
+		this.transition = transition;
+		this.previousState = previousState;
+		this.isFollowState = isFollowState;
+	}
+
+	@Override
+	public String toString() {
+		return previousState + " -> " + transition.target;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		InTransition that = (InTransition) o;
+		return isFollowState == that.isFollowState && Objects.equals(transition, that.transition) && Objects.equals(previousState, that.previousState);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(transition, previousState, isFollowState);
+	}
+}

tool/src/org/antlr/v5/automata/optimization/RedundantEpsilonRemover.java
@@ -1,0 +1,183 @@
+/*
+ * Copyright (c) 2012-present The ANTLR Project. All rights reserved.
+ * Use of this file is governed by the BSD 3-clause license that
+ * can be found in the LICENSE.txt file in the project root.
+ */
+
+package org.antlr.v5.automata.optimization;
+
+import org.antlr.v5.runtime.atn.*;
+
+import java.util.*;
+
+public class RedundantEpsilonRemover {
+	public final ATN atn;
+
+	private final ATNOptimizerHelper helper;
+
+	private RedundantEpsilonRemover(ATN atn, ATNOptimizerHelper helper) {
+		this.atn = atn;
+		this.helper = helper;
+	}
+
+	public static int optimize(ATNOptimizerHelper helper) {
+		List<ATNState> removedStates = helper.getRemovedStates();
+		int numberOfRemovedStates = removedStates.size();
+		new RedundantEpsilonRemover(helper.atn, helper).traverseATN();
+		return removedStates.size() - numberOfRemovedStates;
+	}
+
+	private void traverseATN() {
+		Set<ATNState> visitedStates = new HashSet<>();
+		for (RuleStartState start : atn.ruleToStartState) {
+			traverseState(start, visitedStates);
+		}
+	}
+
+	private void traverseState(ATNState state, Set<ATNState> visitedStates) {
+		if (!visitedStates.add(state)) {
+			return;
+		}
+
+		optimizeState(state);
+
+		for (int i = 0; i < state.getNumberOfTransitions(); i++) {
+			Transition transition = state.transition(i);
+			traverseState(transition.target, visitedStates);
+			if (transition instanceof RuleTransition) {
+				traverseState(((RuleTransition) transition).followState, visitedStates);
+			}
+		}
+	}
+
+	private void optimizeState(ATNState state) {
+		if (state instanceof BasicState && state.getNumberOfTransitions() == 1) {
+			Transition outTransition = state.transition(0);
+			Boolean removeInTransition = null;
+
+			List<InTransition> inTransitions = helper.getInTransitions(state);
+			InTransition inTransition0 = inTransitions.get(0);
+
+			if (inTransitions.size() == 1) {
+				// Input:
+				//     ε     t
+				// I1 --> S --> O1
+
+				// Output:
+				//     t
+				// I1 --> O1
+				if (isSafeForRemovingEpsilon(inTransition0.transition) && validateInTransitionForRemoving(inTransition0, outTransition)) {
+					helper.removeInTransitionsForOutStates(state);
+					helper.replaceTransition(inTransition0, outTransition);
+					removeInTransition = true;
+				}
+			}
+
+			if (removeInTransition == null && isSafeForRemovingEpsilon(outTransition)) {
+				// State with several incoming transitions of any kind can be safely removed
+
+				// Input:
+				// I1
+				//    \     ε
+				// I2 -> S --> O1
+				//    /
+				// I3
+
+				// Output:
+				// I1
+				//    \
+				// I2 -> O1
+				//    /
+				// I3
+				helper.removeInTransitionsForOutStates(state);
+				for (InTransition inTransition : inTransitions) {
+					Transition newTransition = cloneTransition(inTransition, outTransition.target);
+					helper.replaceTransition(inTransition, newTransition);
+				}
+				removeInTransition = false;
+			}
+
+			if (removeInTransition != null) {
+				ATNState newState;
+				if (removeInTransition) {
+					newState = inTransition0.previousState;
+				} else {
+					newState = outTransition.target;
+				}
+
+				helper.addReplacement(state, newState);
+			}
+		}
+	}
+
+	private static boolean isSafeForRemovingEpsilon(Transition transition) {
+		return transition instanceof EpsilonTransition && ((EpsilonTransition) transition).outermostPrecedenceReturn() == -1;
+	}
+
+	private static boolean validateInTransitionForRemoving(InTransition inTransition, Transition outTransition) {
+		Boolean onlyEpsilonTransitions = null;
+		ATNState prevState = inTransition.previousState;
+		Transition transitionToRemove = inTransition.transition;
+		for (int i = 0; i < prevState.getNumberOfTransitions(); i++) {
+			Transition t = prevState.transition(i);
+			if (t != transitionToRemove) {
+				if (!t.isEpsilon()) {
+					onlyEpsilonTransitions = false;
+					break;
+				}
+				onlyEpsilonTransitions = true;
+			}
+		}
+
+		if (onlyEpsilonTransitions != null && onlyEpsilonTransitions) {
+			return outTransition instanceof EpsilonTransition;
+		}
+		else {
+			return true;
+		}
+	}
+
+	private static Transition cloneTransition(InTransition inTransition, ATNState newTarget) {
+		Transition transition = inTransition.transition;
+		switch (transition.getSerializationType()) {
+			case Transition.EPSILON:
+				return new EpsilonTransition(newTarget, ((EpsilonTransition) transition).outermostPrecedenceReturn());
+			case Transition.RANGE:
+				RangeTransition rangeTransition = (RangeTransition) transition;
+				return new RangeTransition(newTarget, rangeTransition.from, rangeTransition.to);
+			case Transition.RULE:
+				RuleTransition ruleTransition = (RuleTransition) transition;
+				RuleStartState ruleStart;
+				ATNState followState;
+				if (inTransition.isFollowState) {
+					ruleStart = (RuleStartState) ruleTransition.target;
+					followState = newTarget;
+				}
+				else {
+					ruleStart = (RuleStartState) newTarget;
+					followState = ruleTransition.followState;
+				}
+				return new RuleTransition(ruleStart, ruleTransition.ruleIndex, ruleTransition.precedence, followState);
+			case Transition.PREDICATE:
+				PredicateTransition predicateTransition = (PredicateTransition) transition;
+				return new PredicateTransition(newTarget, predicateTransition.ruleIndex,
+						predicateTransition.predIndex, predicateTransition.isCtxDependent);
+			case Transition.PRECEDENCE:
+				return new PrecedencePredicateTransition(newTarget, ((PrecedencePredicateTransition) transition).precedence);
+			case Transition.ATOM:
+				return new AtomTransition(newTarget, ((AtomTransition) transition).label);
+			case Transition.ACTION:
+				ActionTransition actionTransition = (ActionTransition) transition;
+				return new ActionTransition(newTarget, actionTransition.ruleIndex, actionTransition.actionIndex,
+						actionTransition.isCtxDependent);
+			case Transition.SET:
+				return new SetTransition(newTarget, ((SetTransition) transition).set);
+			case Transition.NOT_SET:
+				return new NotSetTransition(newTarget, ((NotSetTransition) transition).set);
+			case Transition.WILDCARD:
+				return new WildcardTransition(newTarget);
+		}
+		assert true : "Transition should exist";
+		return null;
+	}
+}